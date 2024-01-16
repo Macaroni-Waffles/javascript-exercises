6d81be57fc0bced468de9e0c9af28ae2f0a2b86a
macaroni-waffles <115866814+Macaroni-Waffles@users.noreply.github.com>
2024-01-16 20:15:34 +0000

    Edit js code and testing code to test

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,12 @@
-const repeatString = function() {
-
-};
+const repeatString = function(string, num) {
+    let bigString = "";
+    if (num < 0)
+        return "ERROR";
+    for (let i = 0; i < num; i++) {
+        bigString += string;
+    }
+    return bigString;
+}
 
 // Do not edit below this line
 module.exports = repeatString;
