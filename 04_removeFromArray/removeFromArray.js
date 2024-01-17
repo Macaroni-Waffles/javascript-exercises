3a81ca132e73c61150bdd973fe277ea363df1006
macaroni-waffles <115866814+Macaroni-Waffles@users.noreply.github.com>
2024-01-17 15:09:18 +0000

    Remove a single item from an array using filter of array

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,12 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(array, toRemove) {
+    let arrayRemoved = array.filter(function (element) {
+        return element !== toRemove;
+    });
+    return arrayRemoved;    
 };
+
+
+
 
 // Do not edit below this line
 module.exports = removeFromArray;
