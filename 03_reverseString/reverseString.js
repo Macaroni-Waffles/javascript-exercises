c6ea3223f54e4870ba92f264edab08993436a689
macaroni-waffles <115866814+Macaroni-Waffles@users.noreply.github.com>
2024-01-17 13:06:50 +0000

    Reverse a string, stack methods for cleaner code

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,21 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+ //   let stringArray = string.split("");
+   // stringArrayReverse = stringArray.reverse();
+    //let stringReverse = stringArrayReverse.join("");
+    return string.split("").reverse().join("");
 };
+
+/*
+pseudocode
+
+spilt given string into an array of characters
+reverse the order of the characters
+make the array back into a string? OR can i return an array backwards?
+return string
+
+can stack methods: split, reverse, and join!
+
+*/
 
 // Do not edit below this line
 module.exports = reverseString;
