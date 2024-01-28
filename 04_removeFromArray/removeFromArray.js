b1d013a1d0eac8b07742350802026efc3d87d4f7
macaroni-waffles <115866814+Macaroni-Waffles@users.noreply.github.com>
2024-01-28 11:30:02 +0000

    Code to remove multiple arguments from an array, edit test doc to run all tests

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,12 +1,25 @@
-const removeFromArray = function(array, toRemove) {
-    let arrayRemoved = array.filter(function (element) {
-        return element !== toRemove;
-    });
+const removeFromArray = function(array, ...toRemove) {
+    let arrayRemoved = array.filter(element => !toRemove.includes(element));
     return arrayRemoved;    
-};
+ };
 
-//branch check
+//loop over an array to find any matches
+//take these matches out of the original array
+//return original array minus matches
 
 
 // Do not edit below this line
 module.exports = removeFromArray;
+
+
+/* to remove one argument only
+
+const removeFromArray = function(array, toRemove) {
+    // let toRemove = [...arguments];
+     let arrayRemoved = array.filter(function (element) {
+        return element !== toRemove;
+     });
+     return arrayRemoved;    
+ };
+ 
+ */
