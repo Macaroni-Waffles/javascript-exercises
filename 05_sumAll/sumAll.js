5b2d3ffe8ba5a852199414c1f52fbc70977547ee
macaroni-waffles <115866814+Macaroni-Waffles@users.noreply.github.com>
2024-02-04 10:00:07 +0000

    Return error with non-number parameters

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,36 @@
-const sumAll = function() {
-
+const sumAll = function(firstNumber, secondNumber) {
+    if (Number.isInteger(firstNumber) && Number.isInteger(secondNumber)) {
+        let finalSum = 0;
+        if (firstNumber < secondNumber) {
+            for (let i = firstNumber; i <= secondNumber; i++) {
+                finalSum += i;
+            }
+            if (finalSum < 0) {
+                return "ERROR";
+            }
+            else {
+                return finalSum;
+            }
+        }
+        else {
+            for (let i = secondNumber; i <= firstNumber; i++) {
+                finalSum += i;
+        }
+            if (finalSum < 0) {
+                return "ERROR";
+            }
+            else {
+                return finalSum;
+            }
+        }
+    
+    }
+    else {
+        return "ERROR";
+    }
 };
+
+
 
 // Do not edit below this line
 module.exports = sumAll;
