312bff6d2e327ddfbb0467739331f3b7caa5b69f
macaroni-waffles <115866814+Macaroni-Waffles@users.noreply.github.com>
2024-03-12 19:35:22 +0000

    Add code to check for simple palindrome - not working

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,13 @@
-const palindromes = function () {
-
+const palindromes = function(word) {
+    let wordAsArray = Array.from(word); // w, o, r, d
+    let reverseWord = wordAsArray.reverse(); //d, r, o, w
+    reverseWord = reverseWord.toString(); //drow
+    if (word === reverseWord) {
+        return false;
+    }
+    else {
+        return true;
+    }
 };
 
 // Do not edit below this line
