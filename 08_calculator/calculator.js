f619ef642671255567fb3c2b47db79ae9eb7df2e
macaroni-waffles <115866814+Macaroni-Waffles@users.noreply.github.com>
2024-03-12 16:38:46 +0000

    Use reduce and map to create a simple calculator, factorial does not work for 0

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,40 @@
-const add = function() {
-	
+// takes only 2 numbers
+const add = function(number1, number2) {
+  return number1 + number2;
 };
 
-const subtract = function() {
-	
+// takes only 2 numbers
+const subtract = function(number1, number2) {
+	return number1 - number2;
 };
 
-const sum = function() {
-	
+// Takes each number in the array and adds it to the total, starting at 0.
+const sum = function(array) {
+	let answer = array.reduce((total, number) => total + number, 0);
+  return answer;
 };
 
-const multiply = function() {
-
+const multiply = function(array) {
+  let answer = array.reduce((total, number) => total * number);
+  return answer;
 };
 
-const power = function() {
-	
+// takes only 2 numbers
+const power = function(number1, number2) {
+	return number1 ** number2;
+  // Should have been:
+  // return Math.pow(number1, number2);
 };
 
-const factorial = function() {
-	
+// factorial = number * (number -1) * (number -2), etc. e.g factorial of 5 is 5 * 4 * 3 * 2 * 1 which equals 120.
+const factorial = function(number) {
+  // Create an array of length of "number", fill that array starting at index 0, map onto that array incrementing by 1.
+  let arrayOfNumber = Array(number).fill(0).map((value, index) => index + 1);
+  // Reverse the array
+  arrayOfNumber = arrayOfNumber.reverse();
+  // multiply each number in the array by the total
+  let answer = arrayOfNumber.reduce((total, num) => total * num);
+  return answer;
 };
 
 // Do not edit below this line
