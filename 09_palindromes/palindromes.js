6676d44142f0237e292d3e40e20770d06877fd4c
macaroni-waffles <115866814+Macaroni-Waffles@users.noreply.github.com>
2024-03-13 17:33:39 +0000

    Clean string and check if matches in reverse - not working at all, was working on 6 of 8 tests. Start over!

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,14 +1,29 @@
-const palindromes = function(word) {
-    let wordAsArray = Array.from(word); // w, o, r, d
-    let reverseWord = wordAsArray.reverse(); //d, r, o, w
-    reverseWord = reverseWord.toString(); //drow
-    if (word === reverseWord) {
-        return false;
-    }
-    else {
+const palindromes = function(originalWord) {
+    let lettersAndNumbers = "abcdefghijklmnopqrstuvwxyz0123456789";
+    let lettersAndNumbersArray = Array.from(lettersAndNumbers);
+        console.log(lettersAndNumbersArray);
+        console.log(originalWord);
+    let wordLowerCase = originalWord.toLowerCase();
+      console.log(`wordLowerCase is: ${wordLowerCase}`);
+    let wordAsArray = Array.from(wordLowerCase);
+      console.log(`wordAsArray is: ${wordAsArray}`);
+    let noPunctuation = wordAsArray.filter((character) => lettersAndNumbersArray.includes(character));
+      console.log(`noPunctuation is: ${noPunctuation}`);
+    originalWord = noPunctuation.join("");
+      console.log(`originalWord as string is: ${originalWord}`);
+  //   wordAsArray.replace(/[\W_,\,]/g, "");
+    let reverseWord = noPunctuation.reverse();
+      console.log(`reverseWord is: ${reverseWord}`);
+    reverseWord = reverseWord.join(""); // .join("") creates a continuous string while removing the commas automatically. .string() would leave commas in.
+      console.log(`reverseWord as string is: ${reverseWord}`);
+    
+    if (originalWord === reverseWord) {
         return true;
-    }
-};
+    };
+    else {
+        return false;
+    };
+  };
 
 // Do not edit below this line
 module.exports = palindromes;
