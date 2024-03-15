ee3d60fc093ae768470fb6691cf899b1093a1a35
macaroni-waffles <115866814+Macaroni-Waffles@users.noreply.github.com>
2024-03-15 17:07:47 +0000

    Javascript practice

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,6 +1,24 @@
-const getTheTitles = function() {
-
+const getTheTitles = function(arrayOfBooks) {
+    return arrayOfBooks.map((itemInArray) => itemInArray.title);
 };
+
+
+
+// const books = [
+//     {
+//       title: 'Book',
+//       author: 'Name'
+//     },
+//     {
+//       title: 'Book2',
+//       author: 'Name2'
+//     }
+//   ]
+  
+//   Your job is to write a function that takes the array and returns an array of titles:
+  
+//   getTheTitles(books) // ['Book','Book2']
+
 
 // Do not edit below this line
 module.exports = getTheTitles;
